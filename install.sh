bb580116a6bf64868810da56e0102ec1797a68f6
root <v.gache@gmail.com>
2015-02-23 10:21:08 +0100

    Pimp my git prompt

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cd /tmp
-add-apt-repository ppa:webupd8team/java
 
+#JAVA for phpStorm etc...
+add-apt-repository ppa:webupd8team/java
 apt-get update
 
 apt-get install nodejs npm nodejs-legacy ruby skype apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt php-apc php5-curl php5-cli php5-json php5-intl curl oracle-java7-installer phpmyadmin git vim php5-xdebug php5-xsl php5-xmlrpc filezilla openvpn
@@ -8,7 +9,15 @@ apt-get install nodejs npm nodejs-legacy ruby skype apache2 mysql-server php5-my
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=bin
 
+#COMPOSER
 mv composer.phar /usr/local/bin/composer
+
+#SASS
 gem install sass
+
+#FRONTEND 
 npm install bower -g
 npm install grunt-cli -g
+
+##PIMP MY GIT PROMPT
+git clone https://github.com/arialdomartini/oh-my-git.git ~/.oh-my-git && echo source ~/.oh-my-git/prompt.sh >> ~/.bashrc
