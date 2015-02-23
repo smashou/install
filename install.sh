2621343b8a983f375612689031f75b98da58bcc8
root <v.gache@gmail.com>
2015-02-23 10:55:58 +0100

    Add other tools

--- install.sh
+++ install.sh
@@ -2,13 +2,15 @@ cd /tmp
 
 #JAVA for phpStorm etc...
 add-apt-repository ppa:webupd8team/java
+echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 94558F59
 apt-get update
 
 apt-get install ubuntu-restricted-extras nodejs npm nodejs-legacy ruby skype apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt php-apc php5-curl php5-cli php5-json php5-intl curl oracle-java7-installer phpmyadmin git vim php5-xdebug php5-xsl php5-xmlrpc filezilla openvpn
 /usr/share/doc/libdvdread4/install-css.sh
 
 #Some stuff
-apt-get install gstreamer0.10-plugins-ugly gxine libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+apt-get install gstreamer0.10-plugins-ugly gxine libdvdread4 totem-mozilla icedax tagtool easytag id3tool lame nautilus-script-audio-convert libmad0 mpg321 libavcodec-extra p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller vlc spotify-client
 
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=bin
@@ -42,3 +44,6 @@ gsettings set com.canonical.indicator.session show-real-name-on-panel true
 
 #remove this fucking scrollbar and put a normal one.
 gsettings set com.canonical.desktop.interface scrollbar-mode normal
+
+#Some fun with spotify
+
